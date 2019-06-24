9dbdbaf985a77a344dd197bd24ddbc7c21664b2c
shenghua-cb <shenghua.lian@careerbuilder.com>
2019-06-24 21:47:32 +0800

    init project

---
+++ src/pages/index/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    hello
+    <button @click="scanCode">xxx</button>
+    {{ scanResult }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      scanResult: 'helo'
+    }
+  },
+
+  components: {
+  },
+
+  methods: {
+    scanCode () {
+      var that = this
+
+      wx.scanCode({
+        success (res) {
+          that.scanResult = res.result
+          console.log(res.result)
+          console.log('.........')
+        }
+      })
+
+
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
